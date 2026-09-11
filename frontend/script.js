894e41ead24f71d7ccb567652ae957026642b70d
Student441991 <studentstd10c@gmail.com>
2026-09-11 14:53:18 +0530

    Update BACKEND_URL to production environment

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "https://student-management-system-production-808c.up.railway.app";
+
 
 
 // ======================================
